4cfb7fd3015cf7385d5520faf80f10d97d58b50a
Jeremy Yallop <yallop@gmail.com>
2022-04-01 21:02:30 +0100

    Parameterize ifmem by the char set implementation

--- lib/charmatch.ml
+++ lib/charmatch.ml
@@ -5,47 +5,51 @@
  * See the file LICENSE for details.
  *)
 
-module CharSet = Set.Make(Char)
 type interval = char * char
 
-module Mk =
+module Make(Charset : Set.S with type elt = char) =
 struct
-  open Parsetree
+  module Mk =
+  struct
+    open Parsetree
 
-  let pattern : pattern_desc -> pattern =
-    Ast_helper.Pat.mk
+    let pattern : pattern_desc -> pattern =
+      Ast_helper.Pat.mk
 
-  let expression : expression_desc -> expression =
-    Ast_helper.Exp.mk
+    let expression : expression_desc -> expression =
+      Ast_helper.Exp.mk
 
-  let interval : char -> char -> pattern =
-    fun l h -> if l <> h then pattern @@ Ppat_interval (Pconst_char l, Pconst_char h)
-               else pattern @@ Ppat_constant (Pconst_char l)
+    let interval : char -> char -> pattern =
+      fun l h -> if l <> h then pattern @@ Ppat_interval (Pconst_char l, Pconst_char h)
+                 else pattern @@ Ppat_constant (Pconst_char l)
 
-  let intervals : interval list -> pattern = function
-    | [] -> failwith "empty interval list not supported"
-    | (l,h)::is ->
-       List.fold_left (fun p (l,h) -> pattern @@ Ppat_or (interval l h, p))
-         (interval l h) is
+    let intervals : interval list -> pattern = function
+      | [] -> failwith "empty interval list not supported"
+      | (l,h)::is ->
+         List.fold_left (fun p (l,h) -> pattern @@ Ppat_or (interval l h, p))
+           (interval l h) is
 
-  let case : 'a. pattern -> 'a code -> (char -> 'a) pat_code =
-    fun pat code ->
-    let fv, rhs = Obj.magic code in
-    (Obj.magic (fv, expression (Pexp_fun (Asttypes.Nolabel, None, pat, rhs))))
+    let case : 'a. pattern -> 'a code -> (char -> 'a) pat_code =
+      fun pat code ->
+      let fv, rhs = Obj.magic code in
+      (Obj.magic (fv, expression (Pexp_fun (Asttypes.Nolabel, None, pat, rhs))))
+  end
+
+  let intervals : Charset.t -> interval list =
+    let add c = function
+      | (l,h) :: tail when Char.code c = succ (Char.code h) -> (l, c) :: tail
+      | l -> (c, c) :: l
+    in fun c -> List.rev (Charset.fold add c [])
+
+  let ipat c = Mk.intervals (intervals c)
+
+
+  let ifmem : 'a. char code -> ?otherwise:'a code -> (Charset.t * 'a code) list -> 'a code =
+    fun c ?otherwise cases ->
+     make_match c
+     @@ (List.map (fun (lhs, rhs) -> Mk.case (ipat lhs) rhs) cases
+         @ match otherwise with None -> []
+                              | Some e -> [.< fun _ -> .~e >. [@metaocaml.functionliteral] ])
 end
 
-let intervals : CharSet.t -> interval list =
-  let add c = function
-    | (l,h) :: tail when Char.code c = succ (Char.code h) -> (l, c) :: tail
-    | l -> (c, c) :: l
-  in fun c -> List.rev (CharSet.fold add c [])
-
-let ipat c = Mk.intervals (intervals c)
-
-
-let ifmem : 'a. char code -> ?otherwise:'a code -> (Set.Make(Char).t * 'a code) list -> 'a code =
-  fun c ?otherwise cases ->
-   make_match c
-   @@ (List.map (fun (lhs, rhs) -> Mk.case (ipat lhs) rhs) cases
-       @ match otherwise with None -> []
-                            | Some e -> [.< fun _ -> .~e >. [@metaocaml.functionliteral] ])
+include Make(Set.Make(Char))
